f059db6f7b959b95a3e262674f6bcd636cd25b3a
cjia23 <cjia23@uwo.ca>
2019-11-30 21:30:25 -0500

    I have downloaded data through quandl

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-1. Something fun
+1. Something fun, nothing serious
+
+I always had the idea of implementing python in stock analysis giving the power of computer science and the simple goal of stocks. The goal is just to make money. nothing sepcial.
+Python pandas has a lot of interface to download stock data. 
+Python matplotlib can be used to visualize stocks trend.
+Some machine learning algorithms can be used to test out strategy.
+
+My main idea is one on using the data to discover good stocks and help my own investement. A good stock in my mind must meet the following standard.
+
+1. good fundmental. P/E, P/B and a low debt ratio, decent history of dividend payment.
+2. good product.
+3. good growth prospect or at least stable.
+
+Some of these can be statistically analyzed while some cannot. But I believe that combing the two can help us a lot in discovering potential opportunities and analyzing.
--- utility.py
+++ utility.py
@@ -1,14 +1,37 @@
-import pandas as pd 
+import pandas as pd
+from pandas_datareader import data
 import os 
 from os.path import join
+import datetime
+import quandl
+
+quandl.ApiConfig.api_key = 'FbG6tRQrs3qMtkGBrhZ5'
 
 cwd = os.getcwd()
+quandl_key = 'FbG6tRQrs3qMtkGBrhZ5'
 
+#download the stock data
 def getStockData(ticker_name, start_date, end_date):
-    df = pd.DataFrame()
+    tickers = []
+    tickers.append(ticker_name)
+    data = quandl.get_table('WIKI/PRICES', ticker = tickers, 
+                        qopts = { 'columns': ['ticker', 'date', 'adj_close'] }, 
+                        date = { 'gte': start_date, 'lte': end_date }, 
+                        paginate=True)
+    return data
+    
+
+def writeToFile():
+    pass
 
 def run():
-    pass
+    end_date = datetime.datetime.today().date()
+    start_date = end_date - datetime.timedelta(1000)
+    print(f"The start date is {start_date}")
+    print(f"The end date is {end_date}")
+
+    husky = getStockData('AAPL', start_date, end_date)
+    print(husky)
 
 if __name__ == "__main__":
     run()
