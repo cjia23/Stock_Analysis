91748a21c84f30be526557f10d7b665d71096708
cjia23 <cjia23@uwo.ca>
2019-11-30 20:31:39 -0500

    first commit

---
+++ utility.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import os 
+from os.path import join
+
+cwd = os.getcwd()
+
+def getStockData(ticker_name, start_date, end_date):
+    df = pd.DataFrame()
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    run()
